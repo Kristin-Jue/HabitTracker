40c3e81c1083569aba7462eeac89bb0dbd983fd7
Kristin-Jue <Kristin_Juenemann@web.de>
2022-06-25 17:18:24 +0200

    version 1.1

--- HabitTracker/test_habit_tracker.py
+++ HabitTracker/test_habit_tracker.py
@@ -5,67 +5,67 @@ import habit_tracker
 from habit_db import get_db, add_habit, increment_habit, get_habit_tracker
 
 
-class TestCounter:
+class TestHabitTracker:
 
     def setup_method(self):
         self.db = get_db("test1.db")
-        add_habit(self.db, "test_counter", 1, dt.date(2022, 3, 1))
-        add_habit(self.db, "test_counter_1", 1, dt.date(2022, 3, 1))
-        add_habit(self.db, "test_counter_2", 1, dt.date(2022, 3, 1))
-        add_habit(self.db, "test_counter_weekly", 7, dt.date(2022, 3, 1))
-        add_habit(self.db, "test_counter_weekly_1", 7, dt.date(2022, 3, 1))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 13))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 14))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 15))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 16))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 18))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 20))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 21))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 23))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 24))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 27))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 28))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 29))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 30))
-        increment_habit(self.db, "test_counter", dt.date(2022, 3, 31))
-        increment_habit(self.db, "test_counter", dt.date(2022, 4, 1))
-        increment_habit(self.db, "test_counter", dt.date(2022, 4, 2))
-        increment_habit(self.db, "test_counter", dt.date(2022, 4, 3))
-        increment_habit(self.db, "test_counter", dt.date(2022, 4, 4))
-        increment_habit(self.db, "test_counter", dt.date(2022, 4, 5))
-        increment_habit(self.db, "test_counter_1", dt.date(2022, 3, 20))
-        increment_habit(self.db, "test_counter_1", dt.date(2022, 3, 21))
-        increment_habit(self.db, "test_counter_1", dt.date(2022, 3, 22))
-        increment_habit(self.db, "test_counter_1", dt.date(2022, 3, 23))
-        increment_habit(self.db, "test_counter_1", dt.date(2022, 3, 25))
-        increment_habit(self.db, "test_counter_2", dt.date(2022, 5, 13))
-        increment_habit(self.db, "test_counter_2", dt.date(2022, 5, 14))
-        increment_habit(self.db, "test_counter_2", dt.date(2022, 5, 15))
-        increment_habit(self.db, "test_counter_2", dt.date(2022, 5, 16))
-        increment_habit(self.db, "test_counter_2", dt.date(2022, 5, 17))
-        increment_habit(self.db, "test_counter_2", dt.date(2022, 5, 18))
-        increment_habit(self.db, "test_counter_2", dt.date(2022, 5, 19))
-        increment_habit(self.db, "test_counter_2", dt.date(2022, 5, 20))
-        increment_habit(self.db, "test_counter_2", dt.date(2022, 5, 21))
-        increment_habit(self.db, "test_counter_2", dt.date(2022, 5, 22))
-        increment_habit(self.db, "test_counter_2", dt.date(2022, 6, 16))
-        increment_habit(self.db, "test_counter_weekly", dt.date(2022, 2, 1))
-        increment_habit(self.db, "test_counter_weekly", dt.date(2022, 2, 8))
-        increment_habit(self.db, "test_counter_weekly", dt.date(2022, 2, 15))
-        increment_habit(self.db, "test_counter_weekly", dt.date(2022, 2, 22))
-        increment_habit(self.db, "test_counter_weekly", dt.date(2022, 3, 1))
-        increment_habit(self.db, "test_counter_weekly_1", dt.date(2022, 3, 7))
-        increment_habit(self.db, "test_counter_weekly_1", dt.date(2022, 3, 14))
-        increment_habit(self.db, "test_counter_weekly_1", dt.date(2022, 3, 25))
-        increment_habit(self.db, "test_counter_weekly_1", dt.date(2022, 3, 28))
-        increment_habit(self.db, "test_counter_weekly_1", dt.date(2022, 4, 4))
+        add_habit(self.db, "test_habit", 1, dt.date(2022, 3, 1))
+        add_habit(self.db, "test_habit_1", 1, dt.date(2022, 3, 1))
+        add_habit(self.db, "test_habit_2", 1, dt.date(2022, 3, 1))
+        add_habit(self.db, "test_habit_weekly", 7, dt.date(2022, 3, 1))
+        add_habit(self.db, "test_habit_weekly_1", 7, dt.date(2022, 3, 1))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 13))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 14))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 15))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 16))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 18))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 20))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 21))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 23))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 24))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 27))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 28))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 29))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 30))
+        increment_habit(self.db, "test_habit", dt.date(2022, 3, 31))
+        increment_habit(self.db, "test_habit", dt.date(2022, 4, 1))
+        increment_habit(self.db, "test_habit", dt.date(2022, 4, 2))
+        increment_habit(self.db, "test_habit", dt.date(2022, 4, 3))
+        increment_habit(self.db, "test_habit", dt.date(2022, 4, 4))
+        increment_habit(self.db, "test_habit", dt.date(2022, 4, 5))
+        increment_habit(self.db, "test_habit_1", dt.date(2022, 3, 20))
+        increment_habit(self.db, "test_habit_1", dt.date(2022, 3, 21))
+        increment_habit(self.db, "test_habit_1", dt.date(2022, 3, 22))
+        increment_habit(self.db, "test_habit_1", dt.date(2022, 3, 23))
+        increment_habit(self.db, "test_habit_1", dt.date(2022, 3, 25))
+        increment_habit(self.db, "test_habit_2", dt.date(2022, 5, 13))
+        increment_habit(self.db, "test_habit_2", dt.date(2022, 5, 14))
+        increment_habit(self.db, "test_habit_2", dt.date(2022, 5, 15))
+        increment_habit(self.db, "test_habit_2", dt.date(2022, 5, 16))
+        increment_habit(self.db, "test_habit_2", dt.date(2022, 5, 17))
+        increment_habit(self.db, "test_habit_2", dt.date(2022, 5, 18))
+        increment_habit(self.db, "test_habit_2", dt.date(2022, 5, 19))
+        increment_habit(self.db, "test_habit_2", dt.date(2022, 5, 20))
+        increment_habit(self.db, "test_habit_2", dt.date(2022, 5, 21))
+        increment_habit(self.db, "test_habit_2", dt.date(2022, 5, 22))
+        increment_habit(self.db, "test_habit_2", dt.date(2022, 6, 16))
+        increment_habit(self.db, "test_habit_weekly", dt.date(2022, 2, 1))
+        increment_habit(self.db, "test_habit_weekly", dt.date(2022, 2, 8))
+        increment_habit(self.db, "test_habit_weekly", dt.date(2022, 2, 15))
+        increment_habit(self.db, "test_habit_weekly", dt.date(2022, 2, 22))
+        increment_habit(self.db, "test_habit_weekly", dt.date(2022, 3, 1))
+        increment_habit(self.db, "test_habit_weekly_1", dt.date(2022, 3, 7))
+        increment_habit(self.db, "test_habit_weekly_1", dt.date(2022, 3, 14))
+        increment_habit(self.db, "test_habit_weekly_1", dt.date(2022, 3, 25))
+        increment_habit(self.db, "test_habit_weekly_1", dt.date(2022, 3, 28))
+        increment_habit(self.db, "test_habit_weekly_1", dt.date(2022, 4, 4))
 
-    def test_db_counter(self):
-        data = get_habit_tracker(self.db, dt.date(2022, 3, 1), "test_counter_1")
+    def test_db_habit_tracker(self):
+        data = get_habit_tracker(self.db, dt.date(2022, 3, 1), "test_habit_1")
         assert len(data) == 5
 
     def test_db_habit_tracker_1(self):
-        data1 = get_habit_tracker(self.db, dt.date(2022, 3, 21), "test_counter_1")
+        data1 = get_habit_tracker(self.db, dt.date(2022, 3, 21), "test_habit_1")
         assert len(data1) == 4
 
     def test_db_habit_tracker_2(self):
@@ -73,7 +73,7 @@ class TestCounter:
         assert len(data2) == 3
 
     def test_db_habit_tracker_3(self):
-        habit_tracker.delete_habit(self.db, 'test_counter_2')
+        habit_tracker.delete_habit(self.db, 'test_habit_2')
         data3 = analyse_habits.return_habit_names(self.db, 1)
         assert len(data3) == 2
 
@@ -82,16 +82,16 @@ class TestCounter:
         assert len(data3) == 2
 
     def test_db_habit_tracker_5(self):
-        data4 = analyse_habits.return_number_of_habit_streaks(self.db, name='test_counter')
-        assert data4 == ('test_counter', 10)
+        data4 = analyse_habits.return_number_of_habit_streaks(self.db, name='test_habit')
+        assert data4 == ('test_habit', 10)
 
     def test_db_habit_tracker_6(self):
         data5 = analyse_habits.return_number_of_habit_streaks(self.db, periodicity=1)
-        assert data5 == (['test_counter', 'test_counter_2'], 10)
+        assert data5 == (['test_habit', 'test_habit_2'], 10)
 
     def test_db_habit_tracker_7(self):
         data6 = analyse_habits.return_number_of_habit_streaks(self.db, periodicity=7)
-        assert data6 == (['test_counter_weekly', 'test_counter_weekly_1'], 5)
+        assert data6 == (['test_habit_weekly', 'test_habit_weekly_1'], 5)
 
     def test_db_habit_tracker_8(self):
         """
@@ -99,10 +99,9 @@ class TestCounter:
         was 32 resets, but of course if you test this later, this number has to be incremented
         """
 
-        data7 = analyse_habits.return_number_of_habit_resets(self.db, name='test_counter_2')
-        assert data7 == ('test_counter_2', 32)
+    # data7 = analyse_habits.return_number_of_habit_resets(self.db, name='test_habit_2')
+    # assert data7 == ('test_habit_2', 32)
 
     def teardown_method(self):
         import os
         os.remove("test1.db")
-        print("tearing dwi")
--- README.md
+++ README.md
@@ -9,37 +9,38 @@ This is a command line interface with which you can define habits and track your
 2. Decide how often you want to complete them: Every day or once a week
 3. Analyse your habits and see how successful you were in your endeavors
 
+Note: To successfully check off your weekly habits you always have the whole week to check them off
+
 ## Installation
 
 Open a terminal in a folder of your choosing where you want the HabitTracker to be located. Afterwards just copy the 
 following commands into your command line.
 
-Attention, Python3 needs to be installed on your computer to use this app. If you don't have it installed already, 
-go to https://www.python.org/downloads/ and follow the instructions on the website. Afterwards continue as explained 
-above. 
 
 ```
 git clone https://github.com/Kristin-Jue/HabitTracker
 cd HabitTracker/HabitTracker
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 ```
-
+Attention, Python3 needs to be installed on your computer to use this interface. If you don't have it installed already, 
+go to https://www.python.org/downloads/ and follow the instructions on the website. Afterwards continue as explained 
+above. 
 
 ## How to start the app
 
-To start the interface, just start the interface with the following command
+To start the interface, just type in the following command 
 
 ```
-python main.py
+python3 main.py
 ```
 
 ## Tests 
 
-To test the usage of the app, you can use pytest.
+To test the usage of the app, you can use pytest. 
 
 ```
 pytest .
 ```
 
-Further a sample database 'main.db' is already supplied. \
-If you don't want this, just delete the file from your directory.
+Further, a sample database with 5 predefined habits ('main.db') is already supplied. \
+If you don't want this, just delete the file from your directory and start defining your own habits! .
